01e753c001c42477dec01f15020c1d8556604b00
Hooopo <hoooopo@gmail.com>
2012-01-21 21:43:29 +0800

    update to Rails3.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,15 +24,4 @@ Rubyist::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
-  # TODO remove quiet assets
-  config.after_initialize do |app|
-    app.assets.logger = Logger.new('/dev/null')
-  end
-end
-# TODO remove quiet assets
-Rails::Rack::Logger.class_eval do
-  def before_dispatch_with_quiet_assets(env)
-    before_dispatch_without_quiet_assets(env) unless env['PATH_INFO'].index("/assets/") == 0
-  end
-  alias_method_chain :before_dispatch, :quiet_assets
 end
